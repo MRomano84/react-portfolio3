a50ec2c3aea01660c3df172e6cabdf48624154c0
Matthew Romano <mathiasrok@gmail.com>
2021-05-08 16:04:27 -0400

    preview components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import Projects from './components/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
+      <Projects />
+      <Contact />
     </div>
   );
 }
