14ed6f3dd8ca6e786d35efd2c55549aa60f18cab
Matthew Romano <mathiasrok@gmail.com>
2021-05-08 16:03:48 -0400

    home 'page' as component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './about_me.css';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="container card mb-3">
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img className="image-fluid" src="../../../images/193x273.JPG" alt="Me Fishing" />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">About Me</h5>
+                            <p className="card-text">I'm 36 years old and live in Manchester, NH. I'm originally from Tacoma, WA and moved to New England in May 2014.
+                            I am a veteran of Operation Iraqi Freedom and served 6 years in the Army.
+                            I am currently attending the UNH Coding Bootcamp in hopes of becoming a full-stack web developer.</p>
+                            <p className="card-text"><small className="text-muted">Merrimack River, Hooksett, NH</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
