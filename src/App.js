7824d7ee1e2c48aa8887b3905bf160430cf2098c
Matthew Romano <mathiasrok@gmail.com>
2021-05-09 11:27:02 -0400

    reverted repo way back now trying to catch up...

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Projects from './components/Projects/Projects';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
         <Navbar />
-      <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <Route path="/" exact component={Home} /> 
-          <Route path="/projects" exact component={Projects} /> 
-          <Route path="/contact" exact component={Contact} /> 
-        </Switch>
-      </Router>
+        <Home /> 
+        <Projects /> 
+        <Contact /> 
     </div>
   );
 }
